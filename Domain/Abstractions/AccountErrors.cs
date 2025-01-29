82280e7e067dae85f11540d329e92f64c72cf8f5
pires lufungula <pires00243@gmail.com>
2025-01-29 10:35:42 -0500

    ELGOPLAN-5 : Factoriser les messages d'erreur

--- Domain/Abstractions/AccountErrors.cs
+++ Domain/Abstractions/AccountErrors.cs
@@ -7,6 +7,6 @@
     {
         static public readonly Error USER_NOT_FOUND = new("USER_NOT_FOUND", "User not found.");
         static public readonly Error INVALID_LOGIN_ATTEMPT = new("INVALID_LOGIN_ATTEMPT", "Invalid login attempt.");
-        static public readonly Error USER_IS_LOCKED_OUT = new("USER_IS_LOCKED_OUT", "User account locked out.");
+        static public readonly Error USER_IS_LOCKED_OUT = new("USER_IS_LOCKED_OUT", "User account locked out. Try again in a few minutes.");
     }
 }
--- Domain/Abstractions/ApiResponseErrorMessage.cs
+++ Domain/Abstractions/ApiResponseErrorMessage.cs
@@ -8,5 +8,6 @@ namespace Domain.Abstractions
     {
         static public readonly Error BAD_REQUEST = new ("404", "Bad request.");
         static public readonly Error ERROR_UNDEFINED = new ("ERROR_UNDEFINED", "Undefined Error.");
+        static public readonly Error VALIDATION_FAILED = new ("VALIDATION_FAILED", "One or more validation errors occurred."); 
     }
 }
