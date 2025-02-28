4b6810afeb35cb00afcb0b0e34a55ec7b9e0ef17
pires lufungula <pires00243@gmail.com>
2025-02-28 16:56:09 -0500

    ELGOPLAN-18 Correction de build

--- Application/Helper/Validators/EndDateAfterStartDateAttribute.cs
+++ Application/Helper/Validators/EndDateAfterStartDateAttribute.cs
@@ -1,34 +1,36 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using Application.Helper.Validators;
 
-public class EndDateAfterStartDateAttribute : ValidationAttribute
+namespace Application.Helper.Validators
 {
-    private readonly string _startDatePropertyName;
-
-    public EndDateAfterStartDateAttribute(string startDatePropertyName)
+    public class EndDateAfterStartDateAttribute : ValidationAttribute
     {
-        _startDatePropertyName = startDatePropertyName;
-    }
+        private readonly string _startDatePropertyName;
 
-    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
-    {
-        var instance = validationContext.ObjectInstance;
-        var type = instance.GetType();
-        var startDateProperty = type.GetProperty(_startDatePropertyName);
-
-        if (startDateProperty == null)
-            return new ValidationResult($"Propriété {_startDatePropertyName} introuvable.");
-
-        var startDateValue = (DateOnly?)startDateProperty.GetValue(instance);
-        var endDateValue = (DateOnly?)value;
-
-        if (startDateValue.HasValue && endDateValue.HasValue)
+        public EndDateAfterStartDateAttribute(string startDatePropertyName)
         {
-            if (endDateValue.Value < startDateValue.Value)
+            _startDatePropertyName = startDatePropertyName;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var instance = validationContext.ObjectInstance;
+            var type = instance.GetType();
+            var startDateProperty = type.GetProperty(_startDatePropertyName);
+
+            if (startDateProperty == null)
+                return new ValidationResult($"Propriété {_startDatePropertyName} introuvable.");
+
+            var startDateValue = (DateOnly?)startDateProperty.GetValue(instance);
+            var endDateValue = (DateOnly?)value;
+
+            if (startDateValue.HasValue && endDateValue.HasValue)
             {
-                return new ValidationResult(ValidatorErrorsMessage.END_DATE_MUST_BE_GREATER.Message);
+                if (endDateValue.Value < startDateValue.Value)
+                {
+                    return new ValidationResult(ValidatorErrorsMessage.END_DATE_MUST_BE_GREATER.Message);
+                }
             }
-        } 
-        return ValidationResult.Success;
+            return ValidationResult.Success;
+        }
     }
 }
